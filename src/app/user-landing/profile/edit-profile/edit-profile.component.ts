bd785ffbe91290278b8e6869810ea37392830bde
IgnasViz <ignas.vizbaras@stud.vilniustech.lt>
2024-03-14 20:13:26 +0200

    mobile table version for edit profile and more

--- src/app/user-landing/profile/edit-profile/edit-profile.component.ts
+++ src/app/user-landing/profile/edit-profile/edit-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-profile.component.scss']
 })
 export class EditProfileComponent {
+  constructor(private router: Router) {
+  }
+  days: string[] = [
+    'Pirmadienis', 'Antradienis', 'Trečiadienis', 'Ketvirtadienis', 'Penktadienis', 'Šeštadienis', 'Sekmadienis'
+  ]
 
+  redirectToProfile(saveData: boolean): void{
+    if (saveData){
+
+    }
+    this.router.navigate(['/profile']);
+  }
 }
