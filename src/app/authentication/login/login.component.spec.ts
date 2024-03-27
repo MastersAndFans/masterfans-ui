d0c86b7cb2f14ed0397c587700b41b0aa8e4d4c0
IgnasViz <ignas.vizbaras@stud.vilniustech.lt>
2024-03-27 20:41:32 +0200

    integration test between login and authentication components #MWA-14

--- src/app/authentication/login/login.component.spec.ts
+++ src/app/authentication/login/login.component.spec.ts
@@ -1,27 +1,53 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed} from '@angular/core/testing';
 import {AuthenticationService} from "../authentication-service";
 import { LoginComponent } from './login.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterTestingModule} from "@angular/router/testing";
+import {ProfileComponent} from "../../user-landing/profile/profile.component";
+import {of, throwError} from 'rxjs'
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: AuthenticationService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule],
+      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule,
+        RouterTestingModule.withRoutes([
+          { path: 'profile', component: ProfileComponent }
+        ]),],
       providers: [AuthenticationService]
     })
     .compileComponents();
-
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthenticationService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call AuthenticationService LoginUser method', () => {
+    const spyLoginUser = spyOn(authService, 'LoginUser').and.returnValue(of({}));
+    const email = 'test@example.com';
+    const password = 'password';
+    component.loginForm.setValue({ email, password });
+    component.onSubmit();
+    // @ts-ignore
+    expect(spyLoginUser).toHaveBeenCalledWith({ email, password });
+  });
+
+  it('should handle 401 while calling LoginUser', () => {
+    spyOn(authService, 'LoginUser').and.returnValue(
+      throwError({ status: 401 })
+    );
+    component.onSubmit();
+    expect(component.loginForm.controls['password'].hasError('invalidCredentials')).toBeTrue();
+  });
+
 });
