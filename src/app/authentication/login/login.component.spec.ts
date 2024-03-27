7eec8f665c37d416f9065da32bb7f7907401a888
IgnasViz <ignas.vizbaras@stud.vilniustech.lt>
2024-03-27 13:25:34 +0200

    fixed unit tests errors for login component #MWA-14

--- src/app/authentication/login/login.component.spec.ts
+++ src/app/authentication/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {AuthenticationService} from "../authentication-service";
 import { LoginComponent } from './login.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +10,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule],
+      providers: [AuthenticationService]
     })
     .compileComponents();
 
