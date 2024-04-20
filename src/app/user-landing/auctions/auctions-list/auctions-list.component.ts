3eeaa3fef1737c7c8fe54a1c757f91ee5337b6e0
IgnasViz <ignas.vizbaras@stud.vilniustech.lt>
2024-04-20 13:06:17 +0300

    created forms for search and bidding #MWA-65

--- src/app/user-landing/auctions/auctions-list/auctions-list.component.ts
+++ src/app/user-landing/auctions/auctions-list/auctions-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-auctions-list',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./auctions-list.component.scss']
 })
 export class AuctionsListComponent {
+  bidForm: FormGroup;
+  searchForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.bidForm = this.formBuilder.group({
+      bid: [''],
+    })
+    this.searchForm = this.formBuilder.group({
+      city: [''],
+      category: [''],
+      priceFrom: [''],
+      priceTo: [''],
+    })
+  }
+
+  bid(): void{
+    console.log(this.bidForm);
+  }
+
+  search(): void{
+    console.log(this.searchForm);
+  }
 }
